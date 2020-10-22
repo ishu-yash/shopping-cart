f4e437c8b5a2447d97360331c93128209f1cf06e
Ishan Trivedi <sheshtrivedi8046@gmail.com>
2020-10-22 18:09:15 +0530

    Fix in Devtools

--- src/redux/storeConfig/store.js
+++ src/redux/storeConfig/store.js
@@ -5,12 +5,12 @@ import { compose, applyMiddleware } from "redux";
 import saga from "../sagas/index";
 
 const sagaMiddleWare = createSagaMiddleWare();
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
+  ? window.__REDUX_DEVTOOLS_EXTENSION__()
+  : (f) => f;
 const store = createStore(
   reducer,
-  compose(
-    applyMiddleware(sagaMiddleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(sagaMiddleWare), devTools)
 );
 
 sagaMiddleWare.run(saga);
